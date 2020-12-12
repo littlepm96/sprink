ee9eaa90ad7b0332d52b6f05b3fbe0ff55265ea6
TeraFlipFlop <48834256+TeraFlipFlop@users.noreply.github.com>
2020-12-12 06:44:04 +0100

    user sprink

---
+++ src/test/java/com/example/sprink/SprinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
