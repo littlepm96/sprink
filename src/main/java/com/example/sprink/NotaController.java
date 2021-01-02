012582466ef485bec8ac15b054bba6aa850d6906
TeraFlipFlop <48834256+TeraFlipFlop@users.noreply.github.com>
2021-01-02 18:44:46 +0100

    Revert "utente jwtAuth"
    This reverts commit 3cbcc762ac33193cbd155a2a30b0bfbe14f2a160.

--- src/main/java/com/example/sprink/NotaController.java
+++ src/main/java/com/example/sprink/NotaController.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 import netscape.javascript.JSObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 @Controller // This means that this class is a Controller
 @RequestMapping(path = "/note") // This means URL's start with /demo (after Application path)
  public class NotaController {
@@ -14,7 +17,8 @@ import org.springframework.web.bind.annotation.*;
     private NotaRepository notaRepository;
 
     @PostMapping(path = "/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewNota(@RequestParam String title
+    public @ResponseBody
+    String addNewNota(@RequestParam String title
             , @RequestParam String content, @RequestParam String user) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
@@ -24,7 +28,6 @@ import org.springframework.web.bind.annotation.*;
        n.setIdUser(user);
        n.setTitle(title);
         notaRepository.save(n);
-
         return "Saved";
     }
 
@@ -35,7 +38,4 @@ import org.springframework.web.bind.annotation.*;
         // This returns a JSON or XML with the users
         return notaRepository.findAll();
 
-    }
-
-
-}
+    }}
--- src/main/java/com/example/sprink/UtenteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sprink;
-
-import it.univaq.adm.goodlife.domain.Utente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtenteRepository extends JpaRepository<Utente, Long> {
-
-    Utente findByEmail(String email);
-}
